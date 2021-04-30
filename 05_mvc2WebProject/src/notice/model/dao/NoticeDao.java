72838e64ff3ab83e8c42214f2cb64945e6d73259
heum-ji <dragon2009t@gmail.com>
2021-04-30 11:17:34 +0900

    목록 조회 기능 - paging
    1page 당 보여줄 게시물 설정 후 시작/끝 번호 rnum 사용하여 정렬 후 추출
    테스트용 - list의 타이틀 sout하여 확인 완료
    dao - selectNoticeList 기능 구현

--- 05_mvc2WebProject/src/notice/model/dao/NoticeDao.java
+++ 05_mvc2WebProject/src/notice/model/dao/NoticeDao.java
@@ -1,5 +1,59 @@
 package notice.model.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import common.JdbcTemplate;
+import notice.model.vo.Notice;
+
 public class NoticeDao {
+
+	// 공지사항 목록 불러오기
+	public ArrayList<Notice> selectNoticeList(Connection conn, int start, int end) {
+		PreparedStatement pstmt = null;
+		ResultSet rset = null;
+		String query = "SELECT * FROM(SELECT ROWNUM AS RNUM, N.* FROM (SELECT * FROM NOTICE ORDER BY NOTICE_NO DESC) N) WHERE RNUM BETWEEN ? AND ?";
+		ArrayList<Notice> list = new ArrayList<Notice>();
+
+		try {
+			pstmt = conn.prepareStatement(query);
+			pstmt.setInt(1, start);
+			pstmt.setInt(2, end);
+
+			rset = pstmt.executeQuery();
+
+			while (rset.next()) {
+				list.add(setNotice(rset));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			JdbcTemplate.close(rset);
+			JdbcTemplate.close(pstmt);
+		}
+		return list;
+	}
+
+	// 공지사항 저장용
+	public Notice setNotice(ResultSet rset) {
+		Notice notice = new Notice();
+
+		try {
+			notice.setFilename(rset.getString("filename"));
+			notice.setFilepath(rset.getString("filepath"));
+			notice.setNoticeContent(rset.getString("notice_content"));
+			notice.setNoticeDate(rset.getString("notice_date"));
+			notice.setNoticeNo(rset.getInt("notice_no"));
+			notice.setNoticeTitle(rset.getString("notice_title"));
+			notice.setNoticeWriter(rset.getString("notice_writer"));
+			notice.setRnum(rset.getInt("rnum"));
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return notice;
+	}
 
 }
--- 05_mvc2WebProject/src/notice/model/service/NoticeService.java
+++ 05_mvc2WebProject/src/notice/model/service/NoticeService.java
@@ -4,16 +4,26 @@ import java.sql.Connection;
 import java.util.ArrayList;
 
 import common.JdbcTemplate;
+import notice.model.dao.NoticeDao;
 import notice.model.vo.Notice;
 
 public class NoticeService {
 
 	public ArrayList<Notice> selectNoticeList(int reqPage) {
 		Connection conn = JdbcTemplate.getConnection();
-		
-//		ArrayList<Notice> list = NoticeDao.selectNoticeList(conn, reqPage);
+		// 1. 한페이지에 게시물을 몇개 보여줄지 : 한 페이지당 10개씩 보여줌
+		int numPerPage = 10;
+		// 2. reqPage를 통해서 게시물 시작 rnum / 끝 rnum 계산
+		// 1페이지 : start : 1, end : 10, 2페이지 : start : 11, end : 20
+		int end = reqPage * numPerPage;
+		int start = end - numPerPage + 1;
+		ArrayList<Notice> list = new NoticeDao().selectNoticeList(conn, start, end);
 
-		return null;
+		// 타이틀 확인 테스트용
+		for (Notice n : list) {
+			System.out.println(n.getNoticeTitle());
+		}
+		return list;
 	}
 
 }
